3f9022d3a9142b962bbbc8387b7fe56db655f9c5
Hoosteen0 <113567993+Hoosteen0@users.noreply.github.com>
2022-09-23 22:25:45 -0400

    Add files via upload

--- proj1.go
+++ proj1.go
@@ -1,27 +1,144 @@
+// Author: Justin Swanson, jswanson2020@my.fit.edu
+// Author: Naomi Cotti, ncotti2020@my.fit.edu
+// Course: CSE 4250, Fall 2022
+// Project: Proj1, Manatee Evacuation
+// Implementation: go version xgcc (Ubuntu 4.9.3-13ubuntu2) 4.9.3 linux/amd64
+
 package main
 
 import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-type gender int64
-
+//------------- Begin Make Types------------
 type manatee struct {
-	id     int
-	age    int
+	id int
+	age int
 	weight int
-	mf     gender
+	position int
 }
 
-func newManatee(identifier int, age int, weight int, mf gender) manatee {
-	p := manatee{id: identifier, age: age, weight: weight, mf: mf}
+type pair struct {
+	back manatee 	// the female
+	front manatee 	// the male
+	totalAge int
+}
+//------------- End Make Types------------
+
+func test(male manatee, female manatee) bool {
+	return male.weight <= female.weight
+}
+
+func pop(list []manatee) manatee {
+	// returns the last element of a slice (or an array?)
+	return list[len(list) - 1]
+}
+
+//-------------- Begin Create Instances--------------
+func newManatee(identifier int, age int, weight int, position int) manatee {
+	p := manatee{id: identifier, age: age, weight: weight, position: position}
 	return p
 }
 
+func newPair(male manatee, female manatee) pair {
+	p := pair{front: male, back: female, totalAge: male.age + female.age}
+	return p
+}
+//-------------- End Create Instances--------------
+
+func createPairings(males []manatee, females []manatee) []pair {
+	var stack []manatee // to hold properly placed male manatees
+	var holding []manatee // for manatees not yet placeable in stack (their position is too high)
+	var pairings []pair // output array for pairings, to keep the manatees together
+	i, j, k := 0, 0, 0
+
+
+	for len(males) > 0 {
+		for k = 0; k < len(holding); k++ {
+			if (holding[k].position == i) {
+				// finally push the holding into the stack
+				stack = append(stack, holding[k])
+				i += 1
+			}
+		}
+		male := pop(males) // get male
+		males = males[:len(males) - 1] // shorten the males list to give illusion of popping
+		passed := test(male, females[i]) // test if male is smaller than female
+		if(passed) {
+			// push into stack
+			male.position = i
+			stack = append(stack, male)
+			i += 1
+		} else {
+			// you didnt pass the test, and could be approaching bounds
+			j = tryNext(i, male, females) // searched through remaining position to see if works
+			if (j != -1) {
+				// there is a valid position for the male, update his position and add to holding
+				male.position = j
+				holding = append(holding, male)
+			} else {
+				// not a valid position in the remaining spaces, backtrack, then try to see if male fits where i was
+				newPos := backtrack(i, male, females, stack) // newPos is where you should backtrack to
+				if (newPos == -1) {
+					fmt.Println("impossible")
+					return nil
+				}
+				for (newPos + 1 <= len(stack)) {
+					// while loop to do backtracking
+					poppedMale := pop(stack) // backtrack and see if the would be a better fit in poppedMale's spot
+					poppedMale.position = -1
+					males = append(males, poppedMale) // add the poppedMale back to males
+					// swap back with front to avoid circular backtracking (if it ever happens)
+					males[0], males[len(males) - 1] = males[len(males) - 1], males[0]
+				}
+				male.position = newPos
+				stack = append(stack, male)
+			}
+		}
+	}
+
+	for k = 0; k < len(stack); k++ {
+		// make new pairings using our properly ordered stack, and female slice
+		pairings = append(pairings, newPair(stack[k], females[k]))
+	}
+	return pairings
+}
+
+func backtrack(i int, male manatee, females []manatee, stack []manatee) int {
+	// returns the integer position where backtracking succeeded, or -1 if not
+	for (len(stack) > 0) {
+		pop(stack)
+		stack = stack[:len(stack) - 1]
+		i -= 1
+		passed := test(male, females[i])
+		if (passed) {
+			return i
+		}
+	}
+	// length of stack is 0, check to see if male fits in 0, if not return -1
+	if (test(male, females[0]) == true) {
+		return 0
+	}
+	return -1
+}
+
+func tryNext(i int, male manatee, females []manatee) int {
+	for (i < len(females)) {
+		pass := test(male, females[i])
+		if (pass) {
+			return i
+		}
+		i += 1
+	}
+	return -1
+}
+
+// creates and returns a row of the 2D slice, that we'll later use to make manatees
 func manateeInput(s string) []int {
 	var mTee []int
 	for _, f := range strings.Fields(s) {
@@ -33,12 +150,23 @@ func manateeInput(s string) []int {
 	//num := strings.Fields(s)
 	return mTee
 }
+
 func main() {
 
-	fmt.Println("Input number of manatees in a row")
+	var i, row, col  int = 0, 0, 0
+
+	// make pair stack for later
+	var pairStack []pair
+
+	// tables used in the printing function
+	var outputFemale []manatee
+	var outputMale []manatee
+
+	//------------Begin Handle Input------------------
+	fmt.Print("Input number of manatees of each gender: ")
 	var rowNum int
 	fmt.Scanln(&rowNum) // this lowkey does not matter at all
-	fmt.Println("Input manatee array")
+	fmt.Println("Input manatee array: ")
 
 	scanner := bufio.NewScanner(os.Stdin)
 	num := make([][]int, 0) // Declare a slice to receive other slices inside it
@@ -52,73 +180,56 @@ func main() {
 			num = append(num, manateeInput(scanner.Text())) // Adding the slice inside list
 		}
 	}
-	for _, value := range num {
+	/*for _, value := range num {
 		fmt.Printf("%d\n", value)
-	}
-	//4fmt.Println(num) // print list
-
-}
-
-/*
-, secondLine, thirdLine, fourthLine
-case 2:
-			secondLine = manateeInput(scanner.Text())
-		case 3:
-			thirdLine = manateeInput(scanner.Text())
-		case 4:
-			fourthLine = manateeInput(scanner.Text())
-fmt.Println(secondLine)
-	fmt.Println(thirdLine)
-	fmt.Println(fourthLine)
-	// probably won't need this if our lists are kept separate. Removing means we take the gender field out of Manatee
-	const (
-		male gender = iota
-		female
-	)
-
-	constant is necessary atm, final product may use a slice instead of an array, just bc i don't know yet if a
-	   constant can be set through user input
-	const numManateesInEachGender int = 4
-	var i, row, col, l int = 0, 0, 0, 0
-
-	// commented out because not currently in use
-	//var stack []manatee
-
-	var input = [numManateesInEachGender][numManateesInEachGender]int{
-		3 2 1 2
-		2 3 4 3
-		2 1 2 1
-		2 2 1 3
-3 2 1 2
-2 3 4 3
-2 1 2 1
-2 2 1 3
+	} */
+	//------------End Handle Input--------------------
 
 	var ListOfFemales []manatee
 	var ListOfMales []manatee
-*/
-/*
-	numManateesInRow := len(input[0]) / 2
-	numManateesInCol := numManateesInEachGender / numManateesInRow
-	half := len(input) / 2 // how many rows before we switch from female to male
 
+	//------------Begin Calculate Row Lengths---------------
+	numManateesInEachGender := rowNum
+	numManateesInRow := len(num[0])/2
+	numManateesInCol := numManateesInEachGender/numManateesInRow
+	half := len(num)/2
+	//------------End Calculate Row Lengths---------------
+
+	//------------Begin Instantiate Manatees--------------
 	i = 1
 	for i <= numManateesInEachGender {
 		for row = 0; row < numManateesInCol; row++ {
-			for col = 0; col <= numManateesInRow; col += 2 {
-				ListOfFemales = append(ListOfFemales, newManatee(i, input[row][col], input[row][col+1], female))
-				ListOfMales = append(ListOfMales, newManatee(i, input[row+half][col], input[row+half][col+1], male))
+			for col = 0; col <= numManateesInRow; col +=2 {
+				ListOfFemales = append(ListOfFemales, newManatee(i, num[row][col], num[row][col+1], -1))
+				ListOfMales = append(ListOfMales, newManatee(i, num[row+ half][col], num[row+ half][col+1], -1))
 				i += 1
 			}
 		}
 	}
+	//------------End Instantiate Manatees----------------
+
+	//------------Start Making Pairings-------------------
+	pairStack = createPairings(ListOfMales, ListOfFemales)
+	//------------End Making Pairings---------------------
+
+	// sort pairStack by total age to get the lowest aged sets of manatees to the left
+	sort.SliceStable(pairStack, func(i, j int) bool { return pairStack[i].totalAge < pairStack[j].totalAge })
+
 	// print out the list of females and then the list of males
-	fmt.Println("Females: ")
-	for l = 0; l < len(ListOfFemales); l++ {
-		fmt.Println(ListOfFemales[l])
+	if (pairStack != nil) {
+		for i = 0; i < len(pairStack); i++ {
+			outputFemale = append(outputFemale, pairStack[i].back)
+			outputMale = append(outputMale, pairStack[i].front)
+		}
+		for i = 0; i < len(pairStack); i++ {
+			fmt.Printf("%d ", outputFemale[i].id)
+		}
+		fmt.Println()
+		for i = 0; i < len(pairStack); i++ {
+			fmt.Printf("%d ", outputMale[i].id)
+		}
+		fmt.Println()
 	}
-	fmt.Println("Males: ")
-	for l = 0; l < len(ListOfMales); l++ {
-		fmt.Println(ListOfMales[l])
-	}
-*/
+}
+
+
